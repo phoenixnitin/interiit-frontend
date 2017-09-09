b7d959857fce87148d81116d57e7ff5adf2801e9
Shiv Tavker <tavkershiv@gmail.com>
2017-09-09 12:06:07 +0530

    routing added

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>Inter IIT Front-End</h1>'
+  template: `<a [routerLink]="['/home']">Home</a> <br /> <a [routerLink]="['/contactus']" >ContactUs</a> 
+                  <router-outlet></router-outlet>
+  `
 })
 export class AppComponent { }
