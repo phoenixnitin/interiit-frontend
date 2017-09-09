217c24f3fadfe7cac0930d4457da7fa473b65a83
Shiv Tavker <tavkershiv@gmail.com>
2017-09-09 12:04:20 +0530

    added home and contact us component

---
+++ app/contactus/contactus.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-contactus',
+    template:'<h1>Contact Us page goes here</h1>'
+})
+
+export class ContactUsComponent{
+
+}
---
+++ app/home/home.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    template:'<h1>Inter IIT Frontend</h1>'
+})
+
+export class HomeComponent{
+
+}
