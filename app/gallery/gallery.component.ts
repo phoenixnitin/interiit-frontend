7b1e83d7f53671972f9850e1a815dba0bb3a0229
Shiv Tavker <tavkershiv@gmail.com>
2017-09-22 02:20:18 +0530

    Gallery Component with child routes

---
+++ app/gallery/gallery.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'gallery',
+    templateUrl: './app/gallery/gallery.html',
+    styleUrls: ['./app/assets/css/gallery.css']
+})
+
+export class GalleryComponent{
+
+}
