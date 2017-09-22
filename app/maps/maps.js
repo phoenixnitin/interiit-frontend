3363829ff14c42cfd6e2843a92e58cc4fe61e8bc
tkmahesh <hsehamtk2299@gmail.com>
2017-09-22 10:40:56 +0530

    Changed the maps a bit

--- app/maps/maps.js
+++ app/maps/maps.js
@@ -1,1 +1,4 @@
-angular.module('InstiMap',['ngmap']);
+angular.module('InstiMap', ['uiGmapgoogle-maps'])
+    .controller('InstiMapController', function($scope) {
+        $scope.map = { center: { latitude: 12.9952, longitude: 80.2380 }, zoom: 8 };
+    });
