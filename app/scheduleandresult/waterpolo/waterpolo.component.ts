d522d2cfc8b140cbbeb2096288d56114b1e98063
Nitin <nitinkumar6912@gmail.com>
2017-09-25 16:57:55 +0530

    change: button active and hover color, button size too

--- app/scheduleandresult/waterpolo/waterpolo.component.ts
+++ app/scheduleandresult/waterpolo/waterpolo.component.ts
@@ -18,12 +18,17 @@ export class WaterPoloComponent implements OnInit, AfterViewInit{
             var heading_height = jQuery('.display-space h2').height();
             var nav_height = jQuery('.display-space nav').height();
             jQuery('.display-space iframe').attr('height',window_height-heading_height-nav_height-150);
+            jQuery('.display-space nav #schedule').addClass('active');
 
             jQuery('#schedule').click(function () {
+                jQuery('.display-space nav').children().removeClass('active');
                 jQuery('.display-space iframe').attr('src','http://indiaatsports.com/widget_matches?tournamentid=5541144758845440');
+                jQuery('.display-space nav #schedule').addClass('active');
             });
             jQuery('#pointstable').click(function () {
+                jQuery('.display-space nav').children().removeClass('active');
                 jQuery('.display-space iframe').attr('src','http://indiaatsports.com/widget_pointstable?tournamentid=5541144758845440');
+                jQuery('.display-space nav #pointstable').addClass('active');
             });
         });
     };
