f6434ac8405a7e2b1efd293882cdfa5fa7b9e3e4
Nitin <nitinkumar6912@gmail.com>
2017-09-28 01:49:16 +0530

    fix: iframe size on window resize

--- app/scheduleandresult/waterpolo/waterpolo.component.ts
+++ app/scheduleandresult/waterpolo/waterpolo.component.ts
@@ -14,12 +14,10 @@ export class WaterPoloComponent implements OnInit, AfterViewInit{
     ngOnInit(){};
     ngAfterViewInit(){
         jQuery(document).ready(function () {
-            var window_height = jQuery(window).height();
-            var heading_height = jQuery('.display-space h2').height();
-            var nav_height = jQuery('.display-space nav').height();
-            jQuery('.display-space iframe').attr('height',window_height-heading_height-nav_height-150);
-            jQuery('.display-space nav #schedule').addClass('active');
-
+            framedimen();
+            jQuery(window).resize(function () {
+                framedimen();
+            });
             jQuery('#schedule').click(function () {
                 jQuery('.display-space nav').children().removeClass('active');
                 jQuery('.display-space iframe').attr('src','https://worldatsports.appspot.com/widget_matches?tournamentid=5541144758845440');
@@ -30,6 +28,13 @@ export class WaterPoloComponent implements OnInit, AfterViewInit{
                 jQuery('.display-space iframe').attr('src','https://worldatsports.appspot.com/widget_pointstable?tournamentid=5541144758845440');
                 jQuery('.display-space nav #pointstable').addClass('active');
             });
+            function framedimen() {
+                var window_height = jQuery(window).height();
+                var heading_height = jQuery('.display-space h2').height();
+                var nav_height = jQuery('.display-space nav').height();
+                jQuery('.display-space iframe').attr('height', window_height - heading_height - nav_height - 150);
+                jQuery('.display-space nav #schedule').addClass('active');
+            }
         });
     };
 }
