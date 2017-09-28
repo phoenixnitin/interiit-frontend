c483292e243c06a624ff4a52fefb25c88e2feb43
Nitin <nitinkumar6912@gmail.com>
2017-09-28 11:42:56 +0530

    remove: coordinator contacts

--- app/contactus/contactus.routes.ts
+++ app/contactus/contactus.routes.ts
@@ -4,8 +4,8 @@ import {ContactCoreComponent} from './cores/core.component';
 import {ContactCoordComponent} from './coords/coord.component';
 
 export const CONTACTUS_ROUTES: Routes = [
-    {path: '', redirectTo: 'core', pathMatch: 'full'},
-    {path: 'core', component: ContactCoreComponent},
-    {path: 'coord', component: ContactCoordComponent}
+    //{path: '', redirectTo: 'core', pathMatch: 'full'},
+    {path: '', component: ContactCoreComponent}
+    //{path: 'coord', component: ContactCoordComponent}
     
 ];
