6fef2dd94c7308907835ae27a5dcef710a5e4125
Shiv Tavker <tavkershiv@gmail.com>
2017-09-29 19:30:43 +0530

    Added Social Campaign

---
+++ app/socialcampaign/socialcampaign.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'social-campaign',
+    templateUrl:'./app/socialcampaign/socialcampaign.html',
+    styleUrls: ['./app/assets/css/socialcampaign.css']
+})
+
+export class SocialCampaignComponent{
+
+}
