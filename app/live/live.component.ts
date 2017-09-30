8fc1860697cf601ee01b4fce7deb2ef9e01575c4
Nitin <nitinkumar6912@gmail.com>
2017-10-01 05:13:49 +0530

    live streaming added

--- app/live/live.component.ts
+++ app/live/live.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Http} from '@angular/http';
 
 @Component({
     selector: 'app-live',
@@ -6,16 +7,51 @@ import {Component} from '@angular/core';
     styleUrls:['./app/assets/css/live.css']
 })
 
-export class LiveComponent{
-
-    constructor(){
+export class LiveComponent implements OnInit{
+    data: Array<object>;
+    // status;
+    // title;
+    // participant;
+    // score;
+    // description;
+    ngOnInit(){
+      this.loadlive();
+    };
+    constructor(private _http: Http){
+      let that = this;
       jQuery(document).ready(function () {
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)};
+        function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
         gtag('config', 'UA-74140602-1');
+        // that.loadlive();
+        window.setInterval(function () {
+          that.loadlive();
+        }, 5000);
       });
     }
 
+    loadlive(){
+      this._http.get('https://script.google.com/macros/s/AKfycbygukdW3tt8sCPcFDlkMnMuNu9bH5fpt7bKV50p2bM/exec?id=1aqljoEV1kLxP8ZtzsW3Cqj8-L72Q79trNJcsNM5B_Lo&sheet=livedata')
+                            .subscribe(res => {
+                                this.data = res.json().livedata;
+                                // console.log(this.data);
+                                this.filldata(this.data);
+                            });
+    }
+    filldata(data){
+      if(data[0].Status == 'online') {
+        jQuery('#title h2').html(data[0].SportName);
+        jQuery('#participant h2').html(data[0].Participant);
+        jQuery('#score h2').html(data[0].Score);
+        jQuery('#description h3').html(data[0].Description);
+        jQuery('#livecontainer-alt').addClass('hide');
+        jQuery('#livecontainer').removeClass('hide');
+      }
+      else{
+        jQuery('#livecontainer').addClass('hide');
+        jQuery('#livecontainer-alt').removeClass('hide');
+      }
+    }
 }
--- app/navbar/navbar.component.ts
+++ app/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import * as jQuery from 'jquery';
 import {UpdateService} from './notification.service';
+import {Http} from '@angular/http';
+import {win} from "@angular/platform-browser/src/facade/browser";
 
 @Component({
   selector: 'navbar',
@@ -14,9 +16,16 @@ export class NavbarComponent {
 
     notificationShow = false;
 
-    constructor(private _updateService: UpdateService){
+    constructor(private _updateService: UpdateService, private _http: Http){
         this.Width = window.screen.width;
         this.Height = window.screen.height;
+        let that = this;
+        jQuery(document).ready(function () {
+          that.loadlive();
+          window.setInterval(function () {
+            that.loadlive();
+          }, 5000);
+        });
     }
 
     toggleMenu() {
@@ -25,11 +34,27 @@ export class NavbarComponent {
       jQuery('body').toggleClass('active');
     };
 
-    notification(){
-      this.notificationShow = !this.notificationShow;
-      console.log('Button Pressed!!')
+    // notification(){
+    //   this.notificationShow = !this.notificationShow;
+    //   console.log('Button Pressed!!')
+    // }
+
+    // updates = this._updateService.getupdates();
+
+  loadlive(){
+      this._http.get('https://script.google.com/macros/s/AKfycbygukdW3tt8sCPcFDlkMnMuNu9bH5fpt7bKV50p2bM/exec?id=1aqljoEV1kLxP8ZtzsW3Cqj8-L72Q79trNJcsNM5B_Lo&sheet=livedata')
+                            .subscribe(res => {
+                                 let data = res.json().livedata;
+                                // console.log(this.data);
+                                this.toggleLive(data);
+                            });
     }
-
-    updates = this._updateService.getupdates();
-
+  toggleLive(data){
+    if(data[0].Status == 'online'){
+      jQuery('.live-status').removeClass('color-red').addClass('color-green');
+    }
+    else{
+      jQuery('.live-status').removeClass('color-green').addClass('color-red');
+    }
+  }
 }
