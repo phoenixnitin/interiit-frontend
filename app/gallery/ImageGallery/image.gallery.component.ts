53c739f5da7d03c59c39d90f325a992a970aa6d1
phoenixnitin <nitinkumar6912@gmail.com>
2018-06-21 10:14:44 +0530

    data fetching link updated

--- app/gallery/ImageGallery/image.gallery.component.ts
+++ app/gallery/ImageGallery/image.gallery.component.ts
@@ -22,7 +22,7 @@ showClick = true;
     ngOnInit(){
 
             
-            this._http.get('https://script.google.com/macros/s/AKfycbygukdW3tt8sCPcFDlkMnMuNu9bH5fpt7bKV50p2bM/exec?id=1EcJRWQPx_IEjsq4EBeOoHfSSjqpxbziqdlFm0JsNkeI&sheet=Image')
+            this._http.get('https://script.google.com/macros/s/AKfycbwX7x2MJxOBJe2muzWr1SVS093M7EQzDnnGpi_q5PJw4MBK-uye/exec?id=1EcJRWQPx_IEjsq4EBeOoHfSSjqpxbziqdlFm0JsNkeI&sheet=Image')
                                 .subscribe(res => {
                                     this.imageArray = res.json().Image;
                                     jQuery(document).ready(() => {
