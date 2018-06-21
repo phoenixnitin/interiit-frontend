d676d5c02543492d1d82dd2301fb0967cf31bcf0
phoenixnitin <nitinkumar6912@gmail.com>
2018-06-21 10:15:41 +0530

    data fetching link updated

--- app/contactus/cores/core.component.ts
+++ app/contactus/cores/core.component.ts
@@ -37,7 +37,7 @@ cores: Array<object>;
 
                 });
                 //-----------Main Code for Service---------//
-                this._http.get('https://script.google.com/macros/s/AKfycbygukdW3tt8sCPcFDlkMnMuNu9bH5fpt7bKV50p2bM/exec?id=1WjO92CQ-BLEqGuqZXzSo-wqTx6YE5PZrRpFtSHhxv-c&sheet=Core')
+                this._http.get('https://script.google.com/macros/s/AKfycbwX7x2MJxOBJe2muzWr1SVS093M7EQzDnnGpi_q5PJw4MBK-uye/exec?id=1WjO92CQ-BLEqGuqZXzSo-wqTx6YE5PZrRpFtSHhxv-c&sheet=Core')
                             .subscribe(res => {
                                 this.cores = res.json().Core;
                                 // console.log(this.cores);
